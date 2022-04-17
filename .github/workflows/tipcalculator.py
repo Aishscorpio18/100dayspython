bb7d3305556cf4565662777ab35e8018cb277011
Aishwarya S <88168903+Aishscorpio18@users.noreply.github.com>
2022-04-17 19:46:49 -0400

    Update tipcalculator.py

--- .github/workflows/tipcalculator.py
+++ .github/workflows/tipcalculator.py
@@ -1,5 +1,6 @@
 print("Welcome to the tip calculator!")
 total_bill=input("What was the total bill?")
+#can change type in input function itself.
 tip=input("How much tip would you like to give? 10,12 or 15?")
 split_bill=(input("How many people to split the bill?"))
 tip=int(tip)
